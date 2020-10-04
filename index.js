a905632233ea9f365c9ddeacbc1603de0c40e42a
incentiv <gustavo.26.guimaraes@gmail.com>
2020-10-04 19:56:36 -0300

    activate and deactivate

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     var ref = firebase.storage();
     var storageRef = ref.ref();
 
-    storageRef.child("Cadastro").listAll().then(function(res) {
+    storageRef.child("Ativos/").listAll().then(function(res) {
         res.items.forEach(function(itemRef) {
             fillUserList(itemRef);
         });
@@ -12,11 +12,37 @@ $(document).ready(function(){
         console.log(error);
     });
     
-    
+    var storageRef2 = ref.ref();
+
+    storageRef2.child("Inativos/").listAll().then(function(res) {
+        res.items.forEach(function(itemRef) {
+            fillUserListInactives(itemRef);
+        });
+        console.log(userListInactive);
+    }).catch(function(error) {
+        console.log(error);
+    });
 });
 
 var userList = [];
+var userListInactive = []
 var select = document.getElementById("list");
+var select_inactives = document.getElementById("list_inactive");
+
+function fillUserListInactives(data) {
+    // console.log(data);
+    userList.push({
+        name: data.location.path,
+        value: data.location.path
+    })
+
+    var option = document.createElement("OPTION"),
+    txt = document.createTextNode(data.location.path);
+    option.appendChild(txt);
+    option.setAttribute("value",data.location.path);
+    select_inactives.insertBefore(option,select_inactives.lastChild)
+
+}
 
 function fillUserList(data) {
     // console.log(data);
@@ -33,9 +59,73 @@ function fillUserList(data) {
 
 }
 
+function showOnScreen(data) {
+    var usuario = data;
+    var jsonString = JSON.stringify(usuario, undefined, 2);
+    alert(jsonString);
+}
+
+function showData() {
+    var selected = select.options[select.selectedIndex];
+
+    // Create a reference with an initial file path and name
+    var storage = firebase.storage();
+    var pathReference = storage.ref(selected.value);
+    pathReference.getDownloadURL().then(function(url) {
+        // `url` is the download URL for 'images/stars.jpg'
+      
+        // This can be downloaded directly:
+        var xhr = new XMLHttpRequest();
+        xhr.responseType = 'blob';
+        xhr.onload = function(event) {
+            var blob = xhr.response;
+
+            const reader = new FileReader();
+            reader.addEventListener('loadend', () => {
+                showOnScreen(JSON.parse(reader.result));
+            })
+            reader.readAsText(blob);
+        };
+        xhr.open('GET', 'https://cors-anywhere.herokuapp.com/' + url);
+        xhr.send();
+      
+      }).catch(function(error) {
+        // Handle any errors
+      });
+}
+
+function showDataActivate() {
+    var selected = select_inactives.options[select_inactives.selectedIndex];
+
+    // Create a reference with an initial file path and name
+    var storage = firebase.storage();
+    var pathReference = storage.ref(selected.value);
+    pathReference.getDownloadURL().then(function(url) {
+        // `url` is the download URL for 'images/stars.jpg'
+      
+        // This can be downloaded directly:
+        var xhr = new XMLHttpRequest();
+        xhr.responseType = 'blob';
+        xhr.onload = function(event) {
+            var blob = xhr.response;
+
+            const reader = new FileReader();
+            reader.addEventListener('loadend', () => {
+                showOnScreen(JSON.parse(reader.result));
+            })
+            reader.readAsText(blob);
+        };
+        xhr.open('GET', 'https://cors-anywhere.herokuapp.com/' + url);
+        xhr.send();
+      
+      }).catch(function(error) {
+        // Handle any errors
+      });
+}
+
+
 function sendData() {
     var selected = select.options[select.selectedIndex];
-    console.log(selected.value)
 
     // Create a reference with an initial file path and name
     var storage = firebase.storage();
@@ -55,6 +145,11 @@ function sendData() {
             reader.addEventListener('loadend', () => {
                 deactivate(JSON.parse(reader.result));
                 // console.log(JSON.parse(reader.result));
+                pathReference.delete().then(function() {
+                    console.log("Deletado da lista de ativos");
+                }).catch(function(error) {
+                    console.log("Erro ao deletar");
+                });
             })
             reader.readAsText(blob);
         };
@@ -64,7 +159,43 @@ function sendData() {
       }).catch(function(error) {
         // Handle any errors
       });
+}
 
+function sendDataActivate() {
+    var selected = select_inactives.options[select_inactives.selectedIndex];
+
+    // Create a reference with an initial file path and name
+    var storage = firebase.storage();
+    var pathReference = storage.ref(selected.value);
+    pathReference.getDownloadURL().then(function(url) {
+        // `url` is the download URL for 'images/stars.jpg'
+      
+        // This can be downloaded directly:
+        var xhr = new XMLHttpRequest();
+        xhr.responseType = 'blob';
+        xhr.onload = function(event) {
+            var blob = xhr.response;
+            console.log(blob)
+            console.log(xhr.response)
+
+            const reader = new FileReader();
+            reader.addEventListener('loadend', () => {
+                activate(JSON.parse(reader.result));
+                // console.log(JSON.parse(reader.result));
+                pathReference.delete().then(function() {
+                    console.log("Deletado da lista de inativos");
+                }).catch(function(error) {
+                    console.log("Erro ao deletar");
+                });
+            })
+            reader.readAsText(blob);
+        };
+        xhr.open('GET', 'https://cors-anywhere.herokuapp.com/' + url);
+        xhr.send();
+      
+      }).catch(function(error) {
+        // Handle any errors
+      });
 }
 
 function deactivate (data) {
@@ -78,16 +209,55 @@ function deactivate (data) {
     xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
     xhr.setRequestHeader('Authorization', 'Bearer ' + 'LEKuQBkDmKbtnBsCbQq70wRClD1MMLAmn3GRs5NLWA-FgUecs0ScGf3ebrMtmj28nRNAVI5JneiR4zNPwqZJqRPpXwA1cFyDFMbAR4dhU0vj5A3Obr2cqWGeEkMBmAmFThgJhDKlo1TVNlys7aH8l76kSMWML2p5u48Td2gAqXdXW5epZ30q4IruHooH5QELxfXp61lSxs2TtT4-29k9fxJjHtHgKHEPuu8CT6rH4-q5AdauqZpt3PeomTUvMGPNzLWMFM1T7-GyOE_qXtj3oqWwfjFwSo6iTP6l_IJNhfwt2o6V3CBqpzdaCPYlsYnm');
     xhr.send(jsonString);
-    alert("Sucesso");
+    // alert("Sucesso");
 
     // Create a root reference
-    // var ref = firebase.storage();
-    // var storageRef = ref.ref();
+    var ref = firebase.storage();
+    var storageRef = ref.ref();
 
-    // storageRef.child(usuario.Nome + '_' + usuario.DataNascimento).putString(jsonString, firebase.storage.StringFormat.RAW).then(function(snapshot) {
-    //     console.log('Uploaded string');
-    // }).catch(function(error) {
-    //     console.log(error);
-    // });
+    var marcaDependente = '';
+    if (usuario.BeneficiarioTitular == "") {
+        marcaDependente = "_TITULAR";
+    } else {
+        marcaDependente = "_DEPENDENTE_DO_" + usuario.BeneficiarioTitular;
+    }
+
+    storageRef.child('Inativos/' + usuario.Nome + '_' + usuario.DataNascimento + marcaDependente).putString(jsonString, firebase.storage.StringFormat.RAW).then(function(snapshot) {
+        console.log('Uploaded string');
+    }).catch(function(error) {
+        console.log(error);
+    });
+
+}
+
+function activate (data) {
+    console.log(data);
+    var usuario = data;
+    usuario.StatusBeneficiario = "I";
+    var jsonString = JSON.stringify(usuario, undefined, 2);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://cors-anywhere.herokuapp.com/http://lifemanager.nextplus.com.br:9095/lifemanagerapihomologacao/lmapi/cadastro', true);
+    xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+    xhr.setRequestHeader('Authorization', 'Bearer ' + 'LEKuQBkDmKbtnBsCbQq70wRClD1MMLAmn3GRs5NLWA-FgUecs0ScGf3ebrMtmj28nRNAVI5JneiR4zNPwqZJqRPpXwA1cFyDFMbAR4dhU0vj5A3Obr2cqWGeEkMBmAmFThgJhDKlo1TVNlys7aH8l76kSMWML2p5u48Td2gAqXdXW5epZ30q4IruHooH5QELxfXp61lSxs2TtT4-29k9fxJjHtHgKHEPuu8CT6rH4-q5AdauqZpt3PeomTUvMGPNzLWMFM1T7-GyOE_qXtj3oqWwfjFwSo6iTP6l_IJNhfwt2o6V3CBqpzdaCPYlsYnm');
+    xhr.send(jsonString);
+    // alert("Sucesso");
+
+    // Create a root reference
+    var ref = firebase.storage();
+    var storageRef = ref.ref();
+
+    var marcaDependente = '';
+    if (usuario.BeneficiarioTitular == "") {
+        marcaDependente = "_TITULAR";
+    } else {
+        marcaDependente = "_DEPENDENTE_DO_" + usuario.BeneficiarioTitular;
+    }
+
+    storageRef.child('Ativos/' + usuario.Nome + '_' + usuario.DataNascimento + marcaDependente).putString(jsonString, firebase.storage.StringFormat.RAW).then(function(snapshot) {
+        console.log('Uploaded string');
+    }).catch(function(error) {
+        console.log(error);
+    });
 
 }
